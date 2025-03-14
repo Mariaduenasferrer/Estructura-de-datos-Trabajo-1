b87e000fa1bb1f98d5e7d6d0be43582572a829a9
Mariaduenasferrer <maria.duenas@edu.uah.es>
2025-03-14 13:30:08 +0100

    Terminado

--- Test/java/PilasyColas/ListaSimplementeEnlazada/TestPilayColaTest.java
+++ Test/java/PilasyColas/ListaSimplementeEnlazada/TestPilayColaTest.java
@@ -3,7 +3,7 @@ package PilasyColas.ListaSimplementeEnlazada;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
-class TestPilayCola {
+class TestPilayColaTest {
 
     @Test
     void testPila() {
